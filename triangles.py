d36ff1e3cb5536881cf0124943657541f02dfccb
Dorsa M. Arezooji <60347580+Dorsa-Arezooji@users.noreply.github.com>
2020-06-08 14:50:28 +0100

    Update and rename part3_triangles.py to triangles.py

--- triangles.py
+++ triangles.py
@@ -7,10 +7,6 @@ from pyspark.sql.types import Row
 sc = pyspark.SparkContext()
 sqlContext = pyspark.SQLContext(sc)
 
-def address_split(x):
-	for i in range(len(x[0])):
-		return (x[0][i], 1)
-
 def v_f(x):
 	d = {}
 	d['id'] = x
@@ -25,17 +21,6 @@ def e_f(x):
 	d['relationship'] = x[2]
 	return d
 
-def is_scammer(x):
-	if x[1][0] > 50:
-		if x[1][1]/x[1][0] < 0.05:
-				return True
-	return False
-
-path_scam = "/user/dma30/Project"
-scams_df = sqlContext.read.option('multiLine', True).json(sc.wholeTextFiles(path_scam).values()).drop('id', 'url', 'name','coin', 'description', 'reporter', 'ip', 'nameservers', 'subcategory', 'category', 'status')
-scammers = scams_df.rdd.map(address_split)
-# (address, 1)
-
 path_tra = '/data/ethereum/transactions/part-00729-5ef0a9bd-d53d-4fd8-9a96-e33e04c37eed-c000.csv'
 tra = sc.textFile(path_tra).map(lambda l: (l.split(',')[1], l.split(',')[2], (l.split(',')[3], l.split(',')[6])))
 # (from, to, (val, time))
@@ -47,5 +32,4 @@ e_df = sqlContext.createDataFrame(tra.map(lambda x: Row(**e_f(x))))
 G = GraphFrame(v_df, e_df)
 
 # finding the triangles
-print('-----------------------------------------------------------------')
-triangle = G.find("(a)-[ab]->(b); (b)-[bc]->(c); (c)-[ca]->(a)").rdd.saveAsTextFile('tr')
+triangle = G.find("(a)-[ab]->(b); (b)-[bc]->(c); (c)-[ca]->(a)").rdd.saveAsTextFile('triangles')
